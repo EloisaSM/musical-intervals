002e514201df53fcc34d8b7bc49010f0f21ff252
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-07-07 22:03:10 -0300

    structure for data

---
+++ src/keysAndIntervals/index.js
@@ -0,0 +1,24 @@
+const intervals = [
+  "T",
+  "2b",
+  "2M",
+  "2#",
+  "3b",
+  "3M",
+  "4J",
+  "4#",
+  "5b",
+  "5J",
+  "5#",
+  "6b",
+  "6M",
+  "7b",
+  "7M",
+];
+
+const keys = ["C", "D", "E", "F", "G", "A", "B"];
+
+module.exports = {
+  intervals,
+  keys,
+};
